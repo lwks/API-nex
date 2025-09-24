d0130f5581dd66123e8319ef0a1bd88492bdd392
lwks_ <lwksandrade@gmail.com>
2025-09-23 22:20:01 -0300

    remove endpoint

--- app/src/controllers/api_v1.py
+++ app/src/controllers/api_v1.py
@@ -1,7 +1,3 @@
 from fastapi import APIRouter
-from app.src.controllers.items_controller import router as items_router
-from app.src.controllers.users_controller import router as users_router
 
 api_router = APIRouter()
-api_router.include_router(items_router, prefix="/items", tags=["items"])
-api_router.include_router(users_router, prefix="/users", tags=["users"])
--- app/src/main.py
+++ app/src/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from app.src.helpers.settings import settings
 from app.src.helpers.logging import configure_logging
 from app.src.controllers.api_v1 import api_router
-from app.src.services.repositories.mongo_items_repository import MongoItemsRepository
-from app.src.services.items_service import ItemsService
-from app.src.services.repositories.mongo_users_repository import MongoUsersRepository
-from app.src.services.users_service import UsersService
-import app.src.controllers.items_controller as items_controller
-import app.src.controllers.users_controller as users_controller
 
 app = FastAPI(title=settings.APP_NAME, docs_url="/docs")
 configure_logging()
@@ -26,20 +21,6 @@ async def docs_redirect() -> RedirectResponse:
 async def startup() -> None:
     global mongo_client
     mongo_client = AsyncIOMotorClient(settings.MONGO_URI)
-    items_repo = MongoItemsRepository(mongo_client, settings.MONGO_DB)
-    items_svc = ItemsService(items_repo)
-    users_repo = MongoUsersRepository(mongo_client, settings.MONGO_DB)
-    users_svc = UsersService(users_repo)
-
-    def _items_provider() -> ItemsService:
-        return items_svc
-
-    def _users_provider() -> UsersService:
-        return users_svc
-
-    # simple dependency injection via provider variables
-    items_controller.items_service_provider = _items_provider  # type: ignore[attr-defined]
-    users_controller.users_service_provider = _users_provider  # type: ignore[attr-defined]
 
 
 @app.on_event("shutdown")
